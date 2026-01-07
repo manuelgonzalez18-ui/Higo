9e74ef0b50208e87fff5a34d6926b379bcb6419c
Manuel Gonzalez <manuelgonzalez18@gmail.com>
2026-01-07 14:05:04 -0400

    Add_Residencia_Marina_Caribe

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -57,6 +57,7 @@ export const searchPlaces = async (query, userLocation) => {
                 { title: "Panaderia Bisau", address: "Higuerote, Miranda", lat: 10.48424, lng: -66.09871 },
                 { title: "Alamar", address: "Higuerote, Miranda", lat: 10.412485185404462, lng: -66.1378176707 },
                 { title: "Asocanales", address: "Higuerote, Miranda", lat: 10.498549543479797, lng: -66.1134562060818 },
+                { title: "Residencia Marina Caribe", address: "Higuerote, Miranda", lat: 10.467131341169896, lng: -66.11266963515165 },
                 { title: query + " (Demo)", address: "Higuerote, VE", lat: 10.4806, lng: -66.0987 },
                 { title: "Playa Los Totumos", address: "Higuerote", lat: 10.5123, lng: -66.0712 },
                 { title: "Puerto Encantado", address: "Higuerote", lat: 10.4732, lng: -66.1245 },
@@ -72,6 +73,7 @@ export const searchPlaces = async (query, userLocation) => {
             { title: "Panaderia Bisau", address: "Higuerote, Miranda", lat: 10.48424, lng: -66.09871 },
             { title: "Alamar", address: "Higuerote, Miranda", lat: 10.412485185404462, lng: -66.1378176707 },
             { title: "Asocanales", address: "Higuerote, Miranda", lat: 10.498549543479797, lng: -66.1134562060818 },
+            { title: "Residencia Marina Caribe", address: "Higuerote, Miranda", lat: 10.467131341169896, lng: -66.11266963515165 },
             { title: "Playa Los Totumos", address: "Higuerote, Miranda", lat: 10.5123, lng: -66.0712 },
             { title: "Centro Comercial Flamingo", address: "Higuerote, Miranda", lat: 10.4850, lng: -66.0950 },
             { title: "Club Puerto Azul", address: "Naiguatá", lat: 10.6012, lng: -66.7321 }
