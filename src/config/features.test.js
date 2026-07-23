8eaf833d2b4222e62f57d53fe9d97509a3063164
manuelgonzalez18-ui <manuelgonzalez18@gmail.com>
2026-07-23 16:15:36 -0400

    test(config): isolate boolean parser from Vite env

--- src/config/features.test.js
+++ src/config/features.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { readBoolean } from './features';
+import { readBoolean } from './parseBooleanFlag';
 
 describe('readBoolean', () => {
     it('recognizes enabled values', () => {
